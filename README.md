556ea0fc35c2e2576768a0e432c654d163a3c3e1
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-06-09 07:49:40 -0700

    signals / derived signals / and css, through https://book.leptos.dev/view/02_dynamic_attributes.html#derived-signals

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ working with leptos-rs
 0.001 - small update to see browsers update automatically
 
 0.002 - show error in browser after https://book.leptos.dev/getting_started/leptos_dx.html#1-set-up-console_error_panic_hook
+
+0.003 - css / signals / derived signals - through https://book.leptos.dev/view/02_dynamic_attributes.html#derived-signals
--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,5 +1,51 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!  Whatup!?"</p> })
+    mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    let (x, set_x) = create_signal(0);
+    let double_count = move || count() * 2;
+    view! {
+        <progress
+            max="50"
+            // signals are functions, so `value=count` and `value=move || count.get()`
+            // are interchangeable.
+            value=double_count
+        />
+        <p>
+            "Double Count: "
+            // and again here
+            {double_count}
+        </p>
+        <button
+            on:click={move |_| {
+                set_x.update(|n| *n += 10);
+            }}
+            // set the `style` attribute
+            style="position: absolute"
+            // and toggle individual CSS properties with `style:`
+            style:left=move || format!("{}px", x() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
+            style:max-width="400px"
+            // Set a CSS variable for stylesheet use
+            style=("--columns", x)
+        >
+            "Click to Move"
+        </button>
+        <button
+            on:click=move |_| {
+                // on stable, this is set_count.set(3);
+                set_count.update(|n| *n += 1);
+            }
+            class:red=move || count() % 2 == 1
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {count}
+        </button>
+    }
 }
