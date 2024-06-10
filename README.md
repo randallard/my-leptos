ee21ec669b3cc98c0d87dfc39947f043065dcb3b
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-06-10 13:18:38 -0700

    set up component to take a function

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # my-leptos
 working with leptos-rs
 
+start with 
+
+        trunk serve
+
 # tags
 
 0.000 - clean start after https://book.leptos.dev/getting_started/index.html#hello-world-getting-set-up-for-leptos-csr-development
@@ -10,3 +14,5 @@ working with leptos-rs
 0.002 - show error in browser after https://book.leptos.dev/getting_started/leptos_dx.html#1-set-up-console_error_panic_hook
 
 0.003 - css / signals / derived signals - through https://book.leptos.dev/view/02_dynamic_attributes.html#derived-signals
+
+0.004 - take a function into component
--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -5,47 +5,38 @@ fn main() {
 }
 
 #[component]
-fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let (x, set_x) = create_signal(0);
-    let double_count = move || count() * 2;
+fn ProgressBar(
+    #[prop(default = 100)]
+    max: u16,
+    progress: impl Fn() -> i32 + 'static
+) -> impl IntoView {
     view! {
         <progress
             max="50"
-            // signals are functions, so `value=count` and `value=move || count.get()`
-            // are interchangeable.
-            value=double_count
+            value=progress
+            style="display: block"
         />
-        <p>
-            "Double Count: "
-            // and again here
-            {double_count}
-        </p>
-        <button
-            on:click={move |_| {
-                set_x.update(|n| *n += 10);
-            }}
-            // set the `style` attribute
-            style="position: absolute"
-            // and toggle individual CSS properties with `style:`
-            style:left=move || format!("{}px", x() + 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
-            style:max-width="400px"
-            // Set a CSS variable for stylesheet use
-            style=("--columns", x)
-        >
-            "Click to Move"
-        </button>
+    }
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    let html = "<p>This HTML will be injected.</p>";
+    let double_count = move || count() * 2;
+    view! {
         <button
             on:click=move |_| {
                 // on stable, this is set_count.set(3);
                 set_count.update(|n| *n += 1);
             }
-            class:red=move || count() % 2 == 1
         >
             "Click me: "
             // on stable, this is move || count.get();
             {count}
         </button>
+        <ProgressBar progress=count />
+        <ProgressBar progress=double_count />
+        <div inner_html=html/>
     }
 }
