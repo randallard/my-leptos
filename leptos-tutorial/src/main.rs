83626bc431dfe7443d364b6c77d09b551287834b
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-06-20 07:52:11 -0700

    list of button components

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -6,46 +6,27 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let (x, set_x) = create_signal(0);
-    let double_count = move || count() * 2;
-    view! {
-        <progress
-            max="50"
-            // signals are functions, so `value=count` and `value=move || count.get()`
-            // are interchangeable.
-            value=double_count
-        />
-        <p>
-            "Double Count: "
-            // and again here
-            {double_count}
-        </p>
-        <button
-            on:click={move |_| {
-                set_x.update(|n| *n += 10);
-            }}
-            // set the `style` attribute
-            style="position: absolute"
-            // and toggle individual CSS properties with `style:`
-            style:left=move || format!("{}px", x() + 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
-            style:max-width="400px"
-            // Set a CSS variable for stylesheet use
-            style=("--columns", x)
-        >
-            "Click to Move"
-        </button>
-        <button
-            on:click=move |_| {
-                // on stable, this is set_count.set(3);
-                set_count.update(|n| *n += 1);
-            }
-            class:red=move || count() % 2 == 1
-        >
-            "Click me: "
-            // on stable, this is move || count.get();
-            {count}
-        </button>
-    }
+// create a list of 5 signals
+let length = 5;
+let counters = (1..=length).map(|idx| create_signal(idx));
+
+// each item manages a reactive view
+// but the list itself will never change
+let counter_buttons = counters
+    .map(|(count, set_count)| {
+        view! {
+            <li>
+                <button
+                    on:click=move |_| set_count.update(|n| *n += 1)
+                >
+                    {count}
+                </button>
+            </li>
+        }
+    })
+    .collect_view();
+
+view! {
+    <ul>{counter_buttons}</ul>
+}
 }
