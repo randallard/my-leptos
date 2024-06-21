f1f047f29d32debfd1d8526027c0cc57846e566f
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-06-21 07:27:30 -0700

    Implement communication from child to parent with write signal

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,48 +1,22 @@
 use leptos::*;
 
 #[component]
-fn App() -> impl IntoView {
-    let (value, set_value) = create_signal(Ok(0));
-
-    // when input changes, try to parse a number from the input
-    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
-
+pub fn App() -> impl IntoView {
+    let (toggled, set_toggled) = create_signal(false);
     view! {
-        <h1>"Error Handling"</h1>
-        <label>
-            "Type a number (or something that's not a number!)"
-            <input type="text" on:input=on_input/>
-            // If an `Err(_) had been rendered inside the <ErrorBoundary/>,
-            // the fallback will be displayed. Otherwise, the children of the
-            // <ErrorBoundary/> will be displayed.
-            <ErrorBoundary
-                // the fallback receives a signal containing current errors
-                fallback=|errors| view! {
-                    <div class="error">
-                        <p>"Not a number! Errors: "</p>
-                        // we can render a list of errors
-                        // as strings, if we'd like
-                        <ul>
-                            {move || errors.get()
-                                .into_iter()
-                                .map(|(_, e)| view! { <li>{e.to_string()}</li>})
-                                .collect::<Vec<_>>()
-                            }
-                        </ul>
-                    </div>
-                }
-            >
-                <p>
-                    "You entered "
-                    // because `value` is `Result<i32, _>`,
-                    // it will render the `i32` if it is `Ok`,
-                    // and render nothing and trigger the error boundary
-                    // if it is `Err`. It's a signal, so this will dynamically
-                    // update when `value` changes
-                    <strong>{value}</strong>
-                </p>
-            </ErrorBoundary>
-        </label>
+        <p>"Toggled? " {toggled}</p>
+        <ButtonA setter=set_toggled/>
+    }
+}
+
+#[component]
+pub fn ButtonA(setter: WriteSignal<bool>) -> impl IntoView {
+    view! {
+        <button
+            on:click=move |_| setter.update(|value| *value = !*value)
+        >
+            "Toggle"
+        </button>
     }
 }
 
